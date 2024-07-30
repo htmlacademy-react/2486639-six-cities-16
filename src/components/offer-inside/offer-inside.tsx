4c0e06abe8cfb27c0bc625b2423f903bb3606a55
Klimov Andrey <klimov.andrey.n@yandex.ru>
2024-07-30 19:58:03 +0700

    докоммит

--- src/components/offer-inside/offer-inside.tsx
+++ src/components/offer-inside/offer-inside.tsx
@@ -1,11 +1,18 @@
-import OfferInsideItem from '../offer-inside-item/offer-inside-item';
-
 function OfferInside(): JSX.Element {
   return (
     <div className="offer__inside">
       <h2 className="offer__inside-title">What&apos;s inside</h2>
       <ul className="offer__inside-list">
-        {Array.from({ length: 7 }, (_, index) => <OfferInsideItem key={`Wi-Fi-${index}`} text='Wi-Fi' />)}
+        {
+          Array.from(
+            { length: 7 },
+            (_, index) => (
+              <li key={`Wi-Fi-${index}`} className="offer__inside-item">
+                {'Wi-Fi'}
+              </li>
+            )
+          )
+        }
       </ul>
     </div>
   );
