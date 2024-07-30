255e9538c07b7c93a41e85e2e327c062b7aa7f46
Klimov Andrey <klimov.andrey.n@yandex.ru>
2024-07-30 16:27:21 +0700

    немного укрупнил компоненты

--- src/components/cities/cities.tsx
+++ src/components/cities/cities.tsx
@@ -1,6 +1,6 @@
 import { Offer } from '../../types/offer';
-import PlacesEmpty from '../places-empty/places-empty';
-import PlacesList from '../places-list/places-list';
+import PlacesSorting from '../places-sorting/places-sorting';
+import PlaceCard from '../place-card/place-card';
 
 type CitiesProps = {
   offers: Offer[];
@@ -15,8 +15,20 @@ function Cities({ offers }: CitiesProps): JSX.Element {
         <section className={(isOffersEmpty) ? 'cities__no-places' : 'cities__places places'}>
           {
             isOffersEmpty
-              ? <PlacesEmpty />
-              : <PlacesList offers={offers} />
+              ?
+              <div className="cities__status-wrapper tabs__content">
+                <b className="cities__status">No places to stay available</b>
+                <p className="cities__status-description">We could not find any property available at the moment in Dusseldorf</p>
+              </div>
+              :
+              <>
+                <h2 className="visually-hidden">Places</h2>
+                <b className="places__found">{offers.length} places to stay in Amsterdam</b>
+                <PlacesSorting />
+                <div className="cities__places-list places__list tabs__content">
+                  {offers.map((offer) => <PlaceCard key={offer.id} offer={offer} />)}
+                </div>
+              </>
           }
         </section>
         <div className="cities__right-section">
