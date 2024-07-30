d4dab362934007117cb885c01858a0fb0fffdae1
Klimov Andrey <klimov.andrey.n@yandex.ru>
2024-07-30 19:19:32 +0700

    подготовка компонентов для отзывов

--- src/components/offer-reviews/offer-reviews.tsx
+++ src/components/offer-reviews/offer-reviews.tsx
@@ -1,4 +1,5 @@
 import { Review } from '../../types/review';
+import OfferReviewItem from '../offer-review-item/offer-review-item';
 import OfferReviewsForm from '../offer-reviews-form/offer-reviews-form';
 
 type OfferHostProps = {
@@ -7,33 +8,20 @@ type OfferHostProps = {
 }
 
 function OfferReviews({ reviews, isShowForm = false }: OfferHostProps): JSX.Element {
+  const reviewsCount: number = reviews.length;
+
   return (
     <section className="offer__reviews reviews">
-      <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
-      <ul className="reviews__list">
-        <li className="reviews__item">
-          <div className="reviews__user user">
-            <div className="reviews__avatar-wrapper user__avatar-wrapper">
-              <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar" />
-            </div>
-            <span className="reviews__user-name">
-              Max
-            </span>
-          </div>
-          <div className="reviews__info">
-            <div className="reviews__rating rating">
-              <div className="reviews__stars rating__stars">
-                <span style={{ width: '80%' }}></span>
-                <span className="visually-hidden">Rating</span>
-              </div>
-            </div>
-            <p className="reviews__text">
-              A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
-            </p>
-            <time className="reviews__time" dateTime="2019-04-24">April 2019</time>
-          </div>
-        </li>
-      </ul>
+      <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviewsCount}</span></h2>
+      {
+        !reviewsCount
+          ?
+          null
+          :
+          <ul className="reviews__list">
+            {reviews.map((review) => <OfferReviewItem key={review.id} review={review} />)}
+          </ul>
+      }
       {isShowForm ? <OfferReviewsForm /> : null}
     </section>
   );
