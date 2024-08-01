28100290d9dc3d7d6c5e17ee17608710b900c8ac
Klimov Andrey <klimov.andrey.n@yandex.ru>
2024-08-01 20:42:11 +0700

    укрупнил компоненты

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,20 +1,48 @@
 import { Offer } from '../../types/offer';
 import HeaderAuth from '../../components/header/header-auth';
 import Locations from '../../components/locations/locations';
-import Cities from '../../components/cities/cities';
+import PlacesSorting from '../../components/places-sorting/places-sorting';
+import PlaceCard from '../../components/place-card/place-card';
 
 type MainPageProps = {
   offers: Offer[];
 }
 
 function MainPage({ offers }: MainPageProps): JSX.Element {
+  const isOffersEmpty: boolean = !offers.length;
+
   return (
     <div className="page page--gray page--main">
       <HeaderAuth />
 
       <main className={`page__main page__main--index ${(offers.length) ? '' : 'page__main--index-empty'}`}>
         <Locations />
-        <Cities offers={offers} />
+        <div className="cities">
+          <div className={`cities__places-container container ${(isOffersEmpty) ? 'cities__places-container--empty' : ''}`}>
+            <section className={(isOffersEmpty) ? 'cities__no-places' : 'cities__places places'}>
+              {
+                isOffersEmpty
+                  ?
+                  <div className="cities__status-wrapper tabs__content">
+                    <b className="cities__status">No places to stay available</b>
+                    <p className="cities__status-description">We could not find any property available at the moment in Dusseldorf</p>
+                  </div>
+                  :
+                  <>
+                    <h2 className="visually-hidden">Places</h2>
+                    <b className="places__found">{offers.length} places to stay in Amsterdam</b>
+                    <PlacesSorting />
+                    <div className="cities__places-list places__list tabs__content">
+                      {offers.map((offer) => <PlaceCard key={offer.id} offer={offer} />)}
+                    </div>
+                  </>
+              }
+            </section>
+            <div className="cities__right-section">
+              {isOffersEmpty ? null : <section className="cities__map map"></section>}
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
