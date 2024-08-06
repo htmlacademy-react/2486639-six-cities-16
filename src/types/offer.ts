9687055ba1305fa17f69e3b14415f9aa3e45811b
Klimov Andrey <klimov.andrey.n@yandex.ru>
2024-08-06 21:58:00 +0500

    Описал все типы для данных

--- src/types/offer.ts
+++ src/types/offer.ts
@@ -1,25 +1,28 @@
+import { City } from './city';
+import { Location } from './location';
+import { User } from './user';
+
 export type OfferId = string;
 
-export type Offer = {
+export type BaseOffer = {
   id: OfferId;
   title: string;
   type: string;
   price: number;
-  city: {
-    name: string;
-    location: {
-      latitude: number;
-      longitude: number;
-      zoom: number;
-    };
-  };
-  location: {
-    latitude: number;
-    longitude: number;
-    zoom: number;
-  };
+  city: City;
+  location: Location;
   isFavorite: boolean;
   isPremium: boolean;
   rating: number;
-  previewImage: string;
+};
+
+export type Offer = BaseOffer & { previewImage: string; };
+
+export type OfferInfo = BaseOffer & {
+  description: string;
+  bedrooms: number;
+  goods: string[];
+  host: User;
+  images: string[];
+  maxAdults: number;
 };
--- src/types/review.ts
+++ src/types/review.ts
@@ -1,11 +1,9 @@
+import { User } from './user';
+
 export type Review = {
   id: string;
   date: string;
-  user: {
-    name: string;
-    avatarUrl: string;
-    isPro: boolean;
-  };
+  user: User;
   comment: string;
   rating: number;
 };
