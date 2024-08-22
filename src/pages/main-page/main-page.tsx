c4eaaddd36b28b3f164d6fdfb7b8cdede7dbf6e0
Klimov Andrey <klimov.andrey.n@yandex.ru>
2024-08-22 16:52:04 +0700

    ошибка - имя города при пустом списке предложений

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -72,7 +72,7 @@ function MainPage({ offers }: MainPageProps): JSX.Element {
                   ?
                   <div className="cities__status-wrapper tabs__content">
                     <b className="cities__status">No places to stay available</b>
-                    <p className="cities__status-description">We could not find any property available at the moment in Dusseldorf</p>
+                    <p className="cities__status-description">We could not find any property available at the moment in {currentCityName}</p>
                   </div>
                   :
                   <>
