13e3eb8a79e923f02e2b870c41684cada45c0ecd
Klimov Andrey <klimov.andrey.n@yandex.ru>
2024-08-25 09:41:53 +0700

    читаемость ++

--- src/components/login-form/login-form.tsx
+++ src/components/login-form/login-form.tsx
@@ -15,10 +15,10 @@ function LoginFrom(): JSX.Element {
     evt.preventDefault();
 
     if (loginRef.current !== null && passwordRef.current !== null) {
-      dispatch(loginAction({
-        login: loginRef.current.value,
-        password: passwordRef.current.value
-      }));
+      const login = loginRef.current.value;
+      const password = passwordRef.current.value;
+
+      dispatch(loginAction({ login, password }));
       navigate(AppRoute.Main);
     }
   };
