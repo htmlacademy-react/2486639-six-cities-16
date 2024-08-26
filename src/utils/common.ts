4bd4771b67408f1e69b47ead5a99aec28ef054e6
Klimov Andrey <klimov.andrey.n@yandex.ru>
2024-08-26 23:28:52 +0700

    упростил getRandomArrayElement

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -6,21 +6,6 @@ function addPluralEnding(word: string, count: number): string {
   return `${word}${(count === 1) ? '' : 's'}`;
 }
 
-const getRandomNumber = (minNumber: number = 0, maxNumber: number = 0): number => {
-  const absMinNumber = Math.abs(minNumber);
-  const absMaxNumber = Math.abs(maxNumber);
-
-  const realMinNumber = Math.min(absMinNumber, absMaxNumber);
-  const realMaxNumber = Math.max(absMinNumber, absMaxNumber);
-
-  const startNumber = Math.ceil(realMinNumber);
-  const endNumber = Math.floor(realMaxNumber);
-
-  const result = Math.random() * (endNumber - startNumber + 1) + startNumber;
-
-  return Math.floor(result);
-};
-
-const getRandomArrayElement = <T>(items: T[]) => (items.length === 0) ? null : items[getRandomNumber(0, items.length - 1)];
+const getRandomArrayElement = <T>(items: T[]) => items[Math.floor(Math.random() * items.length)];
 
 export { upFirstLetter, addPluralEnding, getRandomArrayElement };
